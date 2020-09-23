a9d11a3df77bc232b24e576ac9c5bf370b87b4d7
Taewoong Park <taewoong_park@naver.com>
2020-09-23 12:03:31 +0900

    0923 | add mypage draft

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,20 +2,17 @@
   <div>
     <the-header></the-header>
     <div style="height: 1000px;">
-      <test-list></test-list>
     </div>
   </div>
 </template>
 
 <script>
 import TheHeader from '@/components/home/TheHeader'
-import TestList from '@/components/test/TestList'
 export default {
   name: 'Home',
 
   components: {
     'the-header': TheHeader,
-    'test-list': TestList
   }
 }
 </script>
---
+++ frontend/src/views/userpage/UserPageView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="user-view-wrap">
+    <h1>USERPAGE</h1>
+    
+    <div class="user-content-wrap">
+      <div class="user-content-info">
+        유저 사진
+        유저 정보
+      </div>
+      <div class="user-content">
+
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPageView',
+  
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/base';
+
+#user-view-wrap {
+  margin: 50px 10vw 100px;
+}
+
+.user-content-wrap {
+  @extend .flex-center;
+}
+
+.user-content-box {
+  display: grid;
+}
+</style>
