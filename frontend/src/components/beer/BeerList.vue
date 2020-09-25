7ca01c185baad56d4ea348addced5aa56d8fbde0
김성민 <dia321@naver.com>
2020-09-25 03:29:45 +0000

    Update BeerList.vue

--- frontend/src/components/beer/BeerList.vue
+++ frontend/src/components/beer/BeerList.vue
@@ -2,7 +2,7 @@
   <div class="flex-center beer-list-box">
     <div 
       v-for="(item, index) in searchResultArray" :key="index">
-      <beer-list-item :item="item"></beer-list-item>
+      <beer-list-item :item="item" :beerId="item.id"></beer-list-item>
     </div>
   </div>
 </template>
