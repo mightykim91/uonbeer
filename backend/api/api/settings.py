ff56f4740643e3e93a027c1619102e8068f6ffd3
mightykim91 <mightykim91@naver.com>
2020-09-28 18:58:50 +0900

    Add mysql settings

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from . import db_settings
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -94,12 +95,14 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = db_settings.DATABASES
 
 
 # Password validation
