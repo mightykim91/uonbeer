9b5270fb33fd455138688695d3198c9e677a7a5a
mightykim91 <mightykim91@naver.com>
2020-10-06 04:12:31 +0900

    Add face call(prototype)

---
+++ frontend/src/components/facecall/camera.js
@@ -0,0 +1,19 @@
+const panel = document.querySelector('#video-pannel')
+const myVideo = document.createElement('video')
+
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+    video: true, 
+    audio: true,
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+})
+
+function addVideoStream(video, stream){
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    panel.append(video)
+}
