f131872db6c5ab681d86834d7c73a62bdcf0ea25
Taewoong Park <taewoong_park@naver.com>
2020-10-07 21:18:48 +0900

    1007 | update home UI
    Former-commit-id: 2cf0aff78503902591ddb20b792ab1e120b7fd25

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,7 @@ import cookies from 'vue-cookies'
 import VCalendar from 'v-calendar'
 import infiniteScroll from 'vue-infinite-scroll'
 import CountryFlag from 'vue-country-flag'
+import vueParticles from 'vue-particles'
  
 Vue.component('country-flag', CountryFlag)
 
@@ -14,6 +15,7 @@ require('@/assets/scss/main.scss')
 Vue.use(cookies)
 Vue.use(VCalendar)
 Vue.use(infiniteScroll)
+Vue.use(vueParticles)
 
 Vue.config.productionTip = false
 
