c753ea913217747361c9e807eb27371059ead4bb
이원준 <wonjun9090@naver.com>
2020-10-08 03:06:44 +0900

    update recommend
    Former-commit-id: 661257c05ba2697c321fb2ef3bd67cd030b2fbc4

--- backend/api/recommendation/views.py
+++ backend/api/recommendation/views.py
@@ -6,34 +6,35 @@ from beer.models import Beer
 from review.models import ReviewModel
 from beer.serializers import BeerSerializer
 from api.settings import ALGO_PATH
+import random
 # Create your views here.
 
 @api_view(['GET'])
 def svd(request):
-    drink_list = ReviewModel.objects.filter(author = request.user).values_list('beer', flat=True).order_by('-beer')
+    # drink_list = ReviewModel.objects.filter(author = request.user).values_list('beer', flat=True).order_by('-beer')
     # drink_list = [3,8,16,22]
-    candidate_beers = [i for i in range(1,10900) if i not in drink_list] # 후보 beer_id 리스트
+    # candidate_beers = [i for i in range(4,345) if i not in drink_list] # 후보 beer_id 리스트
 
     # print(reviews)
-    with open(ALGO_PATH, 'rb') as file:
-        algo = pickle.load(file)
+    # with open(ALGO_PATH, 'rb') as file:
+        # algo = pickle.load(file)
 
-    predictions = [algo.predict("user_id가 들어갈 예정", beer_id) for beer_id in candidate_beers]
+    # predictions = [algo.predict("user_id가 들어갈 예정", beer_id) for beer_id in candidate_beers]
 
     # est로 부터 정렬하기 위한 함수
-    def sortkey_est(p):
-        return p.est
+    # def sortkey_est(p):
+        # return p.est
 
     # est 내림차순으로 정렬
-    predictions.sort(key=sortkey_est, reverse=True)
+    # predictions.sort(key=sortkey_est, reverse=True)
 
     # 추천 리스트
-    tops = predictions[:50]
+    # tops = predictions[:50]
 
     # 맥주 pk 뽑기
-    pk_list = []
-    for uid,iid,r,est,k in tops:
-        pk_list.append(iid)
+    pk_list = random.sample(range(4,500),9)
+    # for uid,iid,r,est,k in tops:
+    #     pk_list.append(iid)
     # 추천 맥주 가져와서 보내기
     beers = Beer.objects.filter(pk__in=pk_list)
     serializer = BeerSerializer(beers, many=True)
