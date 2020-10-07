d7797012a5ac3627087f2cfc5da4f1bfeac09ae8
Taewoong Park <taewoong_park@naver.com>
2020-10-07 21:18:48 +0900

    1007 | update home UI
    Former-commit-id: a1d62fe85cec426faef07b2e317a0da4ab00b538

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,94 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <div style="height: 1000px;">
+  <div id="header-img" class="flex-center">
+    <div class="header-msg">
+      U WANT BEER. <br>
+      NOW. <br>
+      <p class="header-msg-sm">
+        지금 원하는 맥주를 검색해보세요!
+      </p>
     </div>
+    <div class="search-bar-wrap">
+      <search-bar></search-bar>
+    </div>
+    <vue-particles
+        color="#ffffff"
+        :particleOpacity="1"
+        :particlesNumber="150"
+        shapeType="circle"
+        :particleSize="13"
+        linesColor="#dedede"
+        :linesWidth="1"
+        :lineLinked="false"
+        :lineOpacity="0"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+        style="height: 100%; width: 100%;"
+      >
+      </vue-particles>
   </div>
 </template>
 
 <script>
-import TheHeader from '@/components/home/TheHeader'
-export default {
-  name: 'Home',
+import SearchBar from '@/components/common/SearchBar'
 
+export default {
   components: {
-    'the-header': TheHeader,
+    'search-bar': SearchBar
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '@/assets/style/base';
 
+#header-img {
+  position: relative;
+  flex-direction: column;
+  height: 80vh;
+  background: #333333;
+  // background-image: url('../../assets/img/header.jpg');
+  background-image: $gradient-yellow;
+  background-size: cover;
+}
+
+.header-msg {
+  position: absolute;
+  top: 20vh;
+  margin-bottom: 20px;
+  width: 50vw;
+  font-size: 3rem;
+  font-weight: bolder;
+  text-align: left;
+  color: white;
+  text-shadow: 3px 3px 5px #333333;
+
+  &-sm {
+    font-size: 1rem;
+    text-shadow: none;
+    color: black;
+  }
+}
+
+.search-bar-wrap {
+  position: absolute;
+  top: 40vh;
+}
+@keyframes slide-bg {
+    from {background-position-x: 0%;}
+    from {background-position-x: 100%;}
+}
+
+@media screen and (max-width: 768px) {
+  #header-img {
+    animation: slide-bg 20s linear infinite alternate;
+  }
+
+  .header-msg {
+    font-size: 2rem;
+  }
+}
 </style>
